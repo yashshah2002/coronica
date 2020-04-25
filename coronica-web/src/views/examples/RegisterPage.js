f5796030a05b90092a5fb8326b4ec7731fc8edf5
Harsha Srikara <harshasrikara@gmail.com>
2020-04-25 00:37:44 -0500

    firebase auth complete, logs to console

--- coronica-web/src/views/examples/RegisterPage.js
+++ coronica-web/src/views/examples/RegisterPage.js
@@ -1,22 +1,4 @@
-/*!
-
-=========================================================
-* Paper Kit React - v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-kit-react
-
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React from "react";
+import React, { useState } from "react";
 
 // reactstrap components
 import { Button, Card, Form, Input, Container, Row, Col } from "reactstrap";
@@ -24,8 +6,38 @@ import { Button, Card, Form, Input, Container, Row, Col } from "reactstrap";
 // core components
 import IndexNavbar from "components/Navbars/IndexNavbar";
 import DemoFooter from "components/Footers/DemoFooter";
+import { isPropertySignature } from "typescript";
 
-function RegisterPage() {
+function RegisterPage(props) {
+  //state variables for setting username & password
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  //called when sign in is clicked
+  let authenticate = () => {
+    if(props.firebase.auth().signInWithEmailAndPassword(username, password).catch(err => {
+      console.log("Invalid email or password");
+    }));
+    props.firebase.auth().onAuthStateChanged(user => {
+      //if user got successfully logged in
+      if(user) {
+        setLoggedIn(true);
+        console.log(user);
+      }
+    });
+  };
+
+  //called by onChange for Input field
+  let updateUsername = (userEvent) => {
+    setUsername(userEvent.target.value);
+  };
+
+  //called by onChange for Input field
+  let updatePassword = (passEvent) => {
+    setPassword(passEvent.target.value);
+  };
+
   document.documentElement.classList.remove("nav-open");
   React.useEffect(() => {
     document.body.classList.add("register-page");
@@ -35,11 +47,12 @@ function RegisterPage() {
   });
   return (
     <>
+    {/* {loggedIn ? <IndexNavbar /> : <IndexNavbar />} */}
       <IndexNavbar />
       <div
         className="page-header"
         style={{
-          backgroundImage: "url(" + require("assets/img/login-image.jpg") + ")"
+          backgroundImage: "url(" + require("assets/img/login-image.jpg") + ")",
         }}
       >
         <div className="filter" />
@@ -53,7 +66,7 @@ function RegisterPage() {
                     className="btn-neutral btn-just-icon mr-1"
                     color="facebook"
                     href="#pablo"
-                    onClick={e => e.preventDefault()}
+                    onClick={(e) => e.preventDefault()}
                   >
                     <i className="fa fa-facebook-square" />
                   </Button>
@@ -61,7 +74,7 @@ function RegisterPage() {
                     className="btn-neutral btn-just-icon mr-1"
                     color="google"
                     href="#pablo"
-                    onClick={e => e.preventDefault()}
+                    onClick={(e) => e.preventDefault()}
                   >
                     <i className="fa fa-google-plus" />
                   </Button>
@@ -69,17 +82,32 @@ function RegisterPage() {
                     className="btn-neutral btn-just-icon"
                     color="twitter"
                     href="#pablo"
-                    onClick={e => e.preventDefault()}
+                    onClick={(e) => e.preventDefault()}
                   >
                     <i className="fa fa-twitter" />
                   </Button>
                 </div>
                 <Form className="register-form">
                   <label>Email</label>
-                  <Input placeholder="Email" type="text" />
+                  <Input
+                    value={username}
+                    placeholder="Email"
+                    type="text"
+                    onChange={updateUsername}
+                  />
                   <label>Password</label>
-                  <Input placeholder="Password" type="password" />
-                  <Button block className="btn-round" color="success">
+                  <Input
+                    value={password}
+                    placeholder="Password"
+                    type="password"
+                    onChange={updatePassword}
+                  />
+                  <Button
+                    block
+                    className="btn-round"
+                    color="success"
+                    onClick={authenticate}
+                  >
                     Sign In
                   </Button>
                 </Form>
