bb5d5ae0657380209cb50df5de4ef4460e9062ba
Caitlin Tibbetts <caitlin@tibbettsfamily.com>
2020-04-26 13:05:23 -0500

    Implemented a simple destress clicker

--- coronica-app/screens/Clicker.tsx
+++ coronica-app/screens/Clicker.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {
-  NavigationScreenComponent,
-  NavigationScreenProps,
-  NavigationStackScreenOptions
-} from "react-navigation";
-import { CStyles } from '../CStyles.tsx';
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import { CStyles } from '../CStyles';
+import CIconButton from '../components/CIconButton';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 export default function Clicker({navigation}) {
+  const [clicks, setClicks] = useState(0)
+  const cookie = <Icon name="cookie" size={200} color="#011638" />;
 
   return (
     <View style={CStyles.container}>
-      <Text style={CStyles.titleStyle}>Clicker</Text>
+      <Text style={CStyles.titleStyle}>Destress Clicker!</Text>
+      <Text style={CStyles.textStyle}>Click as much as you can!!!</Text>
+      <CIconButton icon={cookie} onPress={() => {
+        setClicks(clicks+1)
+      }}/>
+      <Text style={CStyles.titleStyle}>{clicks}</Text>
     </View>
   );
 
